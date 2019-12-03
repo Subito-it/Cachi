3b8e6743c74024c06acb8faac62abbc4232dac50
Tomas Camin <tomas.camin@adevinta.com>
2019-12-03 10:27:14 +0100

    Bump version

--- Sources/Cachi/Cachi.swift
+++ Sources/Cachi/Cachi.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Cachi {
-    static let version = "0.1.5"
+    static let version = "0.1.6"
     static let cacheFolderName = ".Cachi"
     static let temporaryFolderUrl: URL = {
         let url = URL(fileURLWithPath: "/tmp").appendingPathComponent("Cachi")

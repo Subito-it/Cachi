b27a20228657f456098321d929f5f5e33d7b4c27
Tomas Camin <tomas.camin@adevinta.com>
2020-12-22 23:02:59 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -10,10 +10,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tcamin/CachiKit", .branch("master")),
+        .package(url: "https://github.com/Subito-it/CachiKit", .branch("master")),
         .package(url: "https://github.com/Subito-it/Bariloche", .branch("master")),
-        .package(url: "https://github.com/vapor/http.git", .branch("master")),
+        .package(url: "https://github.com/tcamin/http.git", .branch("master")),
         .package(url: "https://github.com/tcamin/Vaux", .branch("cachi")),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

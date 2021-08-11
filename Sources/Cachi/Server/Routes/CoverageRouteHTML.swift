4a655d68795bbf1974f48c0a9f2dab8ccc198e70
Tomas Camin <tomas.camin@adevinta.com>
2021-08-11 14:10:57 +0200

    Implement folder coverage

--- Sources/Cachi/Server/Routes/CoverageRouteHTML.swift
+++ Sources/Cachi/Server/Routes/CoverageRouteHTML.swift
@@ -32,11 +32,12 @@ struct CoverageRouteHTML: Routable {
             return promise.succeed(res)
         }
         
-        var parentParameters = ""
-        if let backShowQueryItem = queryItems.first(where: { $0.name == "show" }) {
-            parentParameters = "&\(backShowQueryItem.name)=\(backShowQueryItem.value ?? "")"
+        var queryParameters = ""
+        for queryItem in queryItems {
+            guard queryItem.name != "id" else { continue }
+            queryParameters += "&\(queryItem.name)=\(queryItem.value ?? "")"
         }
-        
+
         let coverageShowFilter = CoverageShowFilter(rawValue: queryItems.first(where: { $0.name == CoverageShowFilter.queryName })?.value ?? "") ?? .files
 
         let document = html {
@@ -44,11 +45,16 @@ struct CoverageRouteHTML: Routable {
                 title("Cachi - Test result")
                 meta().attr("charset", "utf-8")
                 linkStylesheet(url: "/css?main")
-                script(filepath: Filepath(name: "/script?type=coverage-files&id=\(resultBundle.identifier)\(parentParameters)", path: ""))
+                switch coverageShowFilter {
+                case .files:
+                    script(filepath: Filepath(name: "/script?type=coverage-files&id=\(resultBundle.identifier)\(queryParameters)", path: ""))
+                case .folders:
+                    script(filepath: Filepath(name: "/script?type=coverage-folders&id=\(resultBundle.identifier)\(queryParameters)", path: ""))
+                }
             }
             body {
                 div {
-                    div { floatingHeaderHTML(result: resultBundle, coverageShowFilter: coverageShowFilter, parentParameters: parentParameters) }.class("sticky-top").id("top-bar")
+                    div { floatingHeaderHTML(result: resultBundle, coverageShowFilter: coverageShowFilter, queryItems: queryItems) }.class("sticky-top").id("top-bar")
                     
                     div { table(child: {}).id("coverage-table") }
                 }.class("main-container background")
@@ -58,20 +64,37 @@ struct CoverageRouteHTML: Routable {
         return promise.succeed(document.httpResponse())
     }
     
-    private func floatingHeaderHTML(result: ResultBundle, coverageShowFilter: CoverageShowFilter, parentParameters: String) -> HTML {
+    private func floatingHeaderHTML(result: ResultBundle, coverageShowFilter: CoverageShowFilter, queryItems: [URLQueryItem]) -> HTML {
         let resultTitle = result.htmlTitle()
         let resultSubtitle = result.htmlSubtitle()
         let resultDate = DateFormatter.fullDateFormatter.string(from: result.date)
         
         let resultDevice = "\(result.tests.first!.deviceModel) (\(result.tests.first!.deviceOs))"
+        
+        let filteredFolder = queryItems.first(where: { $0.name == "folder" })?.value
+        let isFilteringFolders = filteredFolder != nil
+        var queryParameters = ""
+        for queryItem in queryItems {
+            guard queryItem.name != "id" && queryItem.name != "coverage_show" && queryItem.name != "folder" && (queryItem.name != "q" || isFilteringFolders) else { continue }
+            queryParameters += "&\(queryItem.name)=\(queryItem.value ?? "")"
+        }
                 
         return div {
             div {
                 div {
-                    link(url: "result?id=\(result.identifier)\(parentParameters)") {
-                        image(url: "/image?imageArrorLeft")
-                            .iconStyleAttributes(width: 8)
-                            .class("icon color-svg-text")
+                    if isFilteringFolders {
+                        link(url: "\(self.path)?id=\(result.identifier)\(queryParameters)\(CoverageShowFilter.folders.params())") {
+                            image(url: "/image?imageArrorLeft")
+                                .iconStyleAttributes(width: 8)
+                                .class("icon color-svg-text")
+                        }
+
+                    } else {
+                        link(url: "result?id=\(result.identifier)\(queryParameters)") {
+                            image(url: "/image?imageArrorLeft")
+                                .iconStyleAttributes(width: 8)
+                                .class("icon color-svg-text")
+                        }
                     }
                     resultTitle
                 }.class("header")
@@ -83,13 +106,14 @@ struct CoverageRouteHTML: Routable {
                 div {
                     var blocks = [HTML]()
                     
-                    let filteringFiles = coverageShowFilter == CoverageShowFilter.files
-                    let filteringFolders = coverageShowFilter == CoverageShowFilter.folders
+                    if !isFilteringFolders {
+                        blocks.append(link(url: "\(self.path)?id=\(result.identifier)\(queryParameters)\(CoverageShowFilter.files.params())") { CoverageShowFilter.files.rawValue.capitalized }.class(coverageShowFilter == CoverageShowFilter.files ? "button-selected" : "button"))
+                        blocks.append(link(url: "\(self.path)?id=\(result.identifier)\(queryParameters)\(CoverageShowFilter.folders.params())") { CoverageShowFilter.folders.rawValue.capitalized }.class(coverageShowFilter == CoverageShowFilter.folders ? "button-selected" : "button"))
+                    } else {
+                        blocks.append(span { filteredFolder!; "&nbsp;" }.class("bold"))
+                    }
                     
-                    blocks.append(link(url: "\(self.path)?id=\(result.identifier)\(parentParameters)\(CoverageShowFilter.files.params())") { CoverageShowFilter.files.rawValue.capitalized }.class(filteringFiles ? "button-selected" : "button"))
-                    blocks.append(link(url: "\(self.path)?id=\(result.identifier)\(parentParameters)\(CoverageShowFilter.folders.params())") { CoverageShowFilter.folders.rawValue.capitalized }.class(filteringFolders ? "button-selected" : "button"))
-                    
-                    blocks.append(span { span { "Filter" }.id("filter-placeholder"); input().id("filter-input").attr("placeholder", filteringFiles ? "File name" : "Folder name") }.id("filter-search"))
+                    blocks.append(span { span { "Filter" }.id("filter-placeholder"); input().id("filter-input").attr("placeholder", coverageShowFilter == CoverageShowFilter.files ? "File name" : "Folder name") }.id("filter-search"))
                     
                     return HTMLBuilder.buildBlocks(blocks)
                 }
--- Sources/Cachi/Server/Routes/ScriptRoute.swift
+++ Sources/Cachi/Server/Routes/ScriptRoute.swift
@@ -24,6 +24,13 @@ struct ScriptRoute: Routable {
                let resultBundle = resultBundles.first(where: { $0.identifier == resultIdentifier }) {
                 scriptContent = scriptFilesCoverage(resultBundle: resultBundle)
             }
+        case "coverage-folders":
+            let resultBundles = State.shared.resultBundles
+            
+            if let resultIdentifier = queryItems.first(where: { $0.name == "id" })?.value,
+               let resultBundle = resultBundles.first(where: { $0.identifier == resultIdentifier }) {
+                scriptContent = scriptFoldersCoverage(resultBundle: resultBundle)
+            }
         default:
             break
         }
@@ -128,11 +135,18 @@ struct ScriptRoute: Routable {
                 const filterInput = document.getElementById('filter-input');
                 const inputQuery = filterInput.value ?? '';
 
+                let inputQueryParam = '';
+                if (folder == null) {
+                    inputQueryParam = `&q=${inputQuery}`;
+                } else {
+                    inputQueryParam = `&q=${urlParams.get('q') ?? ''}&qq=${inputQuery}`;
+                }
+
                 var val = '';
                 for (var i = 0; i < filteredItems.length; i++) {
                     const item = filteredItems[i]
                     const rowClass = i % 2 == 0 ? 'even-row' : 'odd-row'
-                    val += `<tr class='${rowClass}'><td class='filename-col'><a href='/html/coverage-file?id=\(resultBundle.identifier)&path=${file.n}&q=${inputQuery}&${parentParams.join('&')}'>${file.n}</a></td><td class='progress-col'><progress value='${file.s.l.p}' max='100'></progress></td><td class='coverage-col color-subtext'>${Number(file.s.l.p).toFixed(1)}%</td><td class='absorbing-column'></td></tr>`;
+                    val += `<tr class='${rowClass}'><td class='filename-col'><a href='/html/coverage-file?id=\(resultBundle.identifier)&path=${item.n}${inputQueryParam}&${parentParams.join('&')}'>${item.n}</a></td><td class='progress-col'><progress value='${item.s.l.p}' max='100'></progress></td><td class='coverage-col color-subtext'>${Number(item.s.l.p).toFixed(1)}%</td><td class='absorbing-column'></td></tr>`;
                 }
 
                 const coverageTable = document.getElementById('coverage-table');
@@ -141,15 +155,22 @@ struct ScriptRoute: Routable {
 
             window.onload = function() {
                 const filterInput = document.getElementById('filter-input');
-                const inputQuery = urlParams.get('q');
-                filterInput.value = inputQuery;
+
+                if (urlParams.has('folder')) {
+                    filterInput.value = urlParams.get('qq') ?? '';
+                } else {
+                    filterInput.value = urlParams.get('q') ?? '';
+                }
+                
+                const event = new Event('input');
+                event.value = filterInput.value;
                 
                 filterInput.addEventListener('input', inputHandler);
-                inputHandler();
+                filterInput.dispatchEvent(event);
             };
         """
         
-        let minifiedFileCoverage = fileCoverage
+        let minifiedCoverage = coverage
             .replacingOccurrences(of: #""files":"#, with: #""f":"#)
             .replacingOccurrences(of: #""filename":"#, with: #""n":"#)
             .replacingOccurrences(of: #""functions":"#, with: #""fn":"#)
@@ -164,6 +185,87 @@ struct ScriptRoute: Routable {
             .replacingOccurrences(of: #""data":"#, with: #""d":"#)
         
         
-        return minifiedFileCoverage
+        return minifiedCoverage
+    }
+    
+    private func scriptFoldersCoverage(resultBundle: ResultBundle) -> String {
+        guard let url = resultBundle.codeCoveragePerFolderJsonUrl,
+              let coverageRaw = try? String(contentsOf: url) else {
+            return "{}"
+        }
+        
+        let coverage = """
+            const data = \(coverageRaw)
+
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+
+            let parentParams = [];
+            urlParams.set('coverage_show', 'files');
+            urlParams.forEach((value,name) => parentParams.push(`${name}=${value}`));
+            parentParams = parentParams.filter(t => !t.startsWith('id=') && !t.startsWith('q=') && !t.startsWith('qq=') && !t.startsWith('path='));
+
+            const inputHandler = function(e) {
+                var filteredItems = [];
+
+                const query = e == null ? '' : e.target.value.toLowerCase();
+
+                const folder = urlParams.get('folder');
+                
+                for (var item of data) {
+                    const matchesQuery = query == null || item.f.toLowerCase().includes(query);
+                    const matchesFolder = folder == null || item.f.startsWith(folder);
+                    if (matchesQuery && matchesFolder) {
+                        filteredItems.push(item);
+                    }
+                }
+
+                filteredItems = filteredItems.sort(function(a,b) {
+                    return b.n - a.n
+                });
+
+                const filterInput = document.getElementById('filter-input');
+                const inputQuery = filterInput.value ?? '';
+
+                let inputQueryParam = '';
+                if (folder == null) {
+                    inputQueryParam = `&q=${inputQuery}`;
+                } else {
+                    inputQueryParam = `&q=${urlParams.get('q') ?? ''}&qq=${inputQuery}`;
+                }
+
+                var val = '';
+                for (var i = 0; i < filteredItems.length; i++) {
+                    const item = filteredItems[i];
+                    const rowClass = i % 2 == 0 ? 'even-row' : 'odd-row'
+                    val += `<tr class='${rowClass}'><td class='filename-col'><a href='/html/coverage?id=\(resultBundle.identifier)&folder=${item.f}${inputQueryParam}&${parentParams.join('&')}'>${item.f}</a></td><td class='progress-col'><progress value='${item.p}' max='100'></progress></td><td class='coverage-col color-subtext'>${Number(item.p).toFixed(1)}%</td><td class='absorbing-column'></td></tr>`;
+                }
+
+                const coverageTable = document.getElementById('coverage-table');
+                coverageTable.innerHTML = val;
+            }
+
+            window.onload = function() {
+                const filterInput = document.getElementById('filter-input');
+
+                if (urlParams.has('folder')) {
+                    filterInput.value = urlParams.get('qq') ?? '';
+                } else {
+                    filterInput.value = urlParams.get('q') ?? '';
+                }
+
+                const event = new Event('input');
+                event.value = filterInput.value;
+                
+                filterInput.addEventListener('input', inputHandler);
+                filterInput.dispatchEvent(event);
+            };
+        """
+        
+        let minifiedCoverage = coverage
+            .replacingOccurrences(of: #""path":"#, with: #""f":"#)
+            .replacingOccurrences(of: #""percent":"#, with: #""p":"#)
+        
+        return minifiedCoverage
     }
 }

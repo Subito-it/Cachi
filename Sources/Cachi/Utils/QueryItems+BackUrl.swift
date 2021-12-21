cb8206e75cf6ff87c1a1a020016130907252d038
Tomas Camin <tomas.camin@adevinta.com>
2021-12-21 08:46:11 +0100

    Fallback to root

--- Sources/Cachi/Utils/QueryItems+BackUrl.swift
+++ Sources/Cachi/Utils/QueryItems+BackUrl.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension Collection where Element == URLQueryItem {
     var backUrl: String {
-        guard let raw = first(where: { $0.name == "back_url" })?.value else { return "" }
-        return String(hexadecimalRepresentation: raw) ?? ""
+        guard let raw = first(where: { $0.name == "back_url" })?.value else { return "/" }
+        return String(hexadecimalRepresentation: raw) ?? "/"
     }
 }

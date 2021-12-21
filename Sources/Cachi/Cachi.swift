2063268e8d1b406ae916ece292f44b11c73dc2df
Tomas Camin <tomas.camin@adevinta.com>
2021-12-21 08:47:15 +0100

    Bump version

--- Sources/Cachi/Cachi.swift
+++ Sources/Cachi/Cachi.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum Cachi {
     static let cacheFolderName = ".Cachi"
-    static let version = "12.5.1"
+    static let version = "13.0.0"
     static let temporaryFolderUrl: URL = {
         let url = URL(fileURLWithPath: "/tmp").appendingPathComponent("Cachi")
         try? FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)

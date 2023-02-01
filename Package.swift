1fedaeb84da2345bb161708f84894219d90527fc
Tomas Camin <tomas.camin@adevinta.com>
2023-02-01 13:52:41 +0100

    Update package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,19 +9,16 @@ let package = Package(
        .macOS(.v10_15)
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Subito-it/CachiKit", .branch("master")),
-        .package(url: "https://github.com/Subito-it/Bariloche", .branch("master")),
-        .package(url: "https://github.com/tcamin/http.git", .branch("master")),
-        .package(url: "https://github.com/tcamin/Vaux", .branch("cachi")),
-        .package(url: "https://github.com/michaeleisel/ZippyJSON", .branch("master")),
+        .package(url: "https://github.com/Subito-it/CachiKit", branch: "master"),
+        .package(url: "https://github.com/Subito-it/Bariloche", branch: "master"),
+        .package(url: "https://github.com/tcamin/http.git", branch: "master"),
+        .package(url: "https://github.com/tcamin/Vaux", branch: "cachi"),
+        .package(url: "https://github.com/michaeleisel/ZippyJSON", branch: "master"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "Cachi",
-            dependencies: ["HTTPKit", "CachiKit", "Bariloche", "Vaux", "ZippyJSON"]),
+            dependencies: [.product(name: "HTTPKit", package: "http"), "CachiKit", "Bariloche", "Vaux", "ZippyJSON"]),
         .testTarget(
             name: "CachiTests",
             dependencies: ["Cachi"]),

c035179a0e554c92d8548144ae96e82f31249216
Tomas Camin <tomas.camin@adevinta.com>
2023-06-11 17:17:01 +0200

    Add link image

--- Sources/Cachi/Server/Routes/ImageRoute.swift
+++ Sources/Cachi/Server/Routes/ImageRoute.swift
@@ -23,6 +23,7 @@ struct ImageRoute: Routable {
         case "imageArrorLeft": imageContent = imageArrorLeft()
         case "imageArrorRight": imageContent = imageArrorRight()
         case "imageArrorDown": imageContent = imageArrorDown()
+        case "imageLink": imageContent = imageLink()
         case "imageView": imageContent = imageView()
         case "imageAttachment": imageContent = imageAttachment()
         case "imageEmpty": imageContent = imageEmpty()
@@ -64,6 +65,10 @@ struct ImageRoute: Routable {
 
     private func imageArrorDown() -> StaticString {
         StaticString(stringLiteral: ##"<svg width="402" height="236" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd"><path d="m966.198 417.475 34.052 34.048 199.61-199.61-199.61-200.026-34.052 34.048 165.772 165.77-165.772 165.77Z" style="fill:#787877;fill-rule:nonzero" transform="rotate(90 708.842 -255.836)"/><path d="m966.198 417.475 34.052 34.048 199.61-199.61-199.61-200.026-34.052 34.048 165.772 165.77-165.772 165.77Z" style="fill:none;fill-rule:nonzero;stroke:#000;stroke-width:1px" transform="rotate(90 708.842 -255.836)"/></svg>"##)
+    }
+    
+    private func imageLink() -> StaticString {
+        StaticString(stringLiteral: ##"<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="m13.723 18.654-3.61 3.609a5.921 5.921 0 0 1-8.378 0A5.878 5.878 0 0 1 0 18.075c0-1.582.615-3.07 1.734-4.189L6.6 9.021c2.355-2.355 6.114-2.262 8.377 0 .453.453.81.973 1.089 1.527l-1.593 1.592a3.86 3.86 0 0 0-.964-1.652c-1.448-1.448-3.93-1.51-5.439-.001l-.001.002-4.867 4.865a3.85 3.85 0 0 0 0 5.44 3.849 3.849 0 0 0 5.442 0l2.425-2.424a7.482 7.482 0 0 0 2.654.284zm.161-16.918L10.31 5.312a7.57 7.57 0 0 1 2.653.283l2.393-2.389a3.85 3.85 0 0 1 5.44-.001 3.85 3.85 0 0 1 0 5.442l-4.831 4.831-.003.002c-1.438 1.437-3.886 1.552-5.439-.002a3.767 3.767 0 0 1-.956-1.643l-.084.068-1.517 1.515c.28.556.635 1.075 1.088 1.528 2.245 2.245 6.004 2.374 8.378 0l4.832-4.831a5.92 5.92 0 0 0-.001-8.377 5.92 5.92 0 0 0-8.379-.002z"/></svg>"##)
     }
 
     private func imageView() -> StaticString {

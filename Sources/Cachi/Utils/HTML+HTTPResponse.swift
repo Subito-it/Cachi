2aab90a37f02dd047da5aa8cec1f1419930c9c65
Tomas Camin <t.camin@gmail.com>
2026-09-22 08:08:47 +0200

    Retain HTML output during rendering

--- Sources/Cachi/Utils/HTML+HTTPResponse.swift
+++ Sources/Cachi/Utils/HTML+HTTPResponse.swift
@@ -1,14 +1,25 @@
 import Vapor
 import Vaux
 
+private final class HTMLResponseOutputStream: TextOutputStream {
+    private var chunks = [String]()
+
+    func write(_ string: String) {
+        chunks.append(string)
+    }
+
+    var value: String {
+        chunks.joined()
+    }
+}
+
 extension HTML {
     func httpResponse() -> Response {
-        var str = ""
-        let vaux = Vaux()
-        vaux.outputLocation = .string(&str)
+        let output = HTMLResponseOutputStream()
+        let vaux = Vaux(output: .custom(output))
         do {
             try vaux.render(self)
-            return Response(body: Response.Body(string: str))
+            return Response(body: Response.Body(string: output.value))
         } catch {
             print("HTML rendering failed: \(error.localizedDescription)")
             return Response(status: .internalServerError, body: Response.Body(stringLiteral: "Ouch..."))
